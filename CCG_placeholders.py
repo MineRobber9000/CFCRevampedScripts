93134a385e2bc9f3ab176c018103289221848eb6
Robert Miles <khuxkm@ttm.sh>
2021-12-29 18:53:55 -0500

    Cleanup CCG_placeholders.py a little

--- CCG_placeholders.py
+++ CCG_placeholders.py
@@ -6,25 +6,29 @@ try: # errors end up in error.txt
         export = json.load(f)
 
     # Figure out where our GIDs start
-    # Since this should be run after the schedule for the season has been generated,
-    # FBGM's "schedule" already has games for us
+    # Since this should be run after the schedule for the season has been
+    # generated, FBGM's "schedule" already has games for us
     GID = max([x["gid"] for x in export["schedule"]])+1
     # Figure out CCG week; just the last week the game generated + 1
-    # The game calls them "days" because all of the other GM games are bolted onto
-    # BBGM's framework, and basketball isn't played in weeks.
+    # The game calls them "days" because all of the other GM games are bolted
+    # onto BBGM's framework, and basketball isn't played in weeks.
     WEEK = max([x["day"] for x in export["schedule"]])+1
 
+    # Get our conferences
+    # Conferences are in gameAttributes as per the manual:
+    # https://football-gm.com/manual/customization/game-attributes/#confs-divs
+    # But for leagues in progress, this stores a history of conferences, so we
+    # need the latest one.
+    CONFS = export["gameAttributes"]["confs"][-1]["value"]
+
+    # Little logging facility (useful for finding bugs)
     _log = []
     def log(*args): _log.append(" ".join(map(str,args)))
 
     # Now generate the placeholders.
     # Simple 0 to n, where n is the number of conferences times 2, minus one
     TEAM = 0
-    # Conferences are in gameAttributes as per the manual:
-    # https://football-gm.com/manual/customization/game-attributes/#confs-divs
-    # But for leagues in progress, this stores a history of conferences, so we
-    # need the latest one.
-    for conference in export["gameAttributes"]["confs"][-1]["value"]:
+    for conference in CONFS:
         log("Creating placeholder CCG for",conference["name"],"Conference")
         game = {}
         game["homeTid"] = TEAM
